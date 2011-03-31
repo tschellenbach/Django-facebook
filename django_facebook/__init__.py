5ea5d19810124d7ffb08fc8068dff913161d9e0e
Thierry Schellenbach <thierryschellenbach@gmail.com>
2011-03-31 11:59:19 +0200

    added twitter to readme

--- django_facebook/__init__.py
+++ django_facebook/__init__.py
@@ -1,10 +1,10 @@
 __author__ = 'Thierry Schellenbach'
 __copyright__ = 'Copyright 2010, Thierry Schellenbach'
-__credits__ = ['Thierry Schellenbach, mellowmorning.com']
+__credits__ = ['Thierry Schellenbach, mellowmorning.com, @tschellenbach']
 
 
 __license__ = 'BSD'
-__version__ = '2.0.4'
+__version__ = '2.0.5'
 __maintainer__ = 'Thierry Schellenbach'
 __email__ = 'thierryschellenbach@gmail.com'
 __status__ = 'Production'
