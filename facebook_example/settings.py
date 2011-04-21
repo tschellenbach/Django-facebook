dab2614e8a4523d5a61a3f0fdb626465bd5f4398
Thierry Schellenbach <thierryschellenbach@gmail.com>
2011-04-21 11:01:20 +0200

    changed the name of the example project

--- facebook_example/settings.py
+++ facebook_example/settings.py
@@ -97,7 +97,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'facebook_example.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
