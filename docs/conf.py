761c00a03c2c147d19e8296b4107da6858b14533
WoLpH <Rick@Fawo.nl>
2011-05-26 22:25:03 +0200

    added API docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,10 @@
 
 import sys, os
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+sys.path.append('..')
+sys.path.append(os.path.join('..', 'facebook_example'))
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
