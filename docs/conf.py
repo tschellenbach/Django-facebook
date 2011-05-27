59540e008a6567cfce1edd3e80002d66dabb2bd6
Thierry Schellenbach <thierryschellenbach@gmail.com>
2011-05-27 18:21:25 +0200

    setting django settings module in conf.py for docs and readthedocs.... is this the solution?

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@
 import sys, os
 
 sys.path.append('..')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'facebook_example.settings'
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
