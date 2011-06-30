a41dd8d53c584f6b2d0f3a56064dfa74a9858128
Thierry Schellenbach <thierryschellenbach@gmail.com>
2011-06-30 18:18:33 +0200

    Fixed a typo

--- django_facebook/tasks.py
+++ django_facebook/tasks.py
@@ -1,26 +1,23 @@
 from celery import task
 import logging
-
 logger = logging.getLogger(__name__)
-
-
 
 
 
 @task.task(ignore_result=True)
 def store_likes(user, likes):
-    from django_facebook.api import FacebookApi
+    from django_facebook.api import FacebookAPI
     logger.info('celery is storing %s likes' % len(likes))
-    FacebookApi._store_likes(user, likes)
+    FacebookAPI._store_likes(user, likes)
     
     return likes
 
 
 @task.task(ignore_result=True)
 def store_friends(user, friends):
-    from django_facebook.api import FacebookApi
+    from django_facebook.api import FacebookAPI
     logger.info('celery is storing %s friends' % len(friends))
-    FacebookApi._store_friends(user, friends)
+    FacebookAPI._store_friends(user, friends)
     
     return friends
 
