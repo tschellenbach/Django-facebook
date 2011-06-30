638bdb33ff2fed52c27f4011e77c2756253897e2
crackcomm <crackcomm@gmail.com>
2011-06-30 11:05:11 -0700

    Deleted not used import's and added "faceboook" to don't add it to context always when needed.

--- django_facebook/context_processors.py
+++ django_facebook/context_processors.py
@@ -1,19 +1,10 @@
-
-from django.conf import settings
-from django.middleware.csrf import get_token
-from django.utils.functional import lazy
-
+from django_facebook.api import get_facebook_graph
 
 def facebook(request):
-    """
-    Context processor that provides a CSRF token, or the string 'NOTPROVIDED' if
-    it has not been provided by either a view decorator or the middleware
-    """
     context = {}
     from django_facebook import settings as facebook_settings
     context['FACEBOOK_API_KEY'] = facebook_settings.FACEBOOK_API_KEY
     context['FACEBOOK_APP_ID'] = facebook_settings.FACEBOOK_APP_ID
+    context['facebook'] = get_facebook_graph(request)
     
     return context
-
-
