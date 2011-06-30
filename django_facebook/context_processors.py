cd70ed687cb043d4ba8cca28a6df4b62f7fcc3a8
crackcomm <crackcomm@gmail.com>
2011-06-30 11:53:48 -0700

    Stupid idea, hitting /me every hit on website...need some cache

--- django_facebook/context_processors.py
+++ django_facebook/context_processors.py
@@ -1,10 +1,7 @@
-from django_facebook.api import get_facebook_graph
-
 def facebook(request):
     context = {}
     from django_facebook import settings as facebook_settings
     context['FACEBOOK_API_KEY'] = facebook_settings.FACEBOOK_API_KEY
     context['FACEBOOK_APP_ID'] = facebook_settings.FACEBOOK_APP_ID
-    context['facebook'] = get_facebook_graph(request)
     
     return context
