4a8f68aaf8f605b9db71a7a9dc7861dd44db8d45
Andrey Makhnach <andrey.makhnach@gmail.com>
2011-09-03 04:17:29 +0800

    Added FACEBOOK_CANVAS_SCOPE

--- django_facebook/canvas.py
+++ django_facebook/canvas.py
@@ -2,9 +2,7 @@ from django.http import QueryDict
 from django_facebook import settings as facebook_settings
 
 
-
-
-def generate_oauth_url(scope='email,read_stream', next=None, extra_data=None):
+def generate_oauth_url(scope=facebook_settings.FACEBOOK_CANVAS_SCOPE, next=None, extra_data=None):
     query_dict = QueryDict('', True)
     canvas_page = next if next is not None else facebook_settings.FACEBOOK_CANVAS_PAGE
     query_dict.update(dict(client_id=facebook_settings.FACEBOOK_APP_ID, redirect_uri=canvas_page, scope=scope))
