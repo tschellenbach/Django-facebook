8437446cdbcd39ba33b0cf356856b15c86bdbf7b
Thierry Schellenbach <thierryschellenbach@gmail.com>
2012-04-23 13:31:24 +0200

    docs for tasks

--- django_facebook/tasks.py
+++ django_facebook/tasks.py
@@ -6,6 +6,16 @@ logger = logging.getLogger(__name__)
 
 @task.task(ignore_result=True)
 def store_likes(user, likes):
+    '''
+    Inserting again will not cause any errors, so this is safe
+    for multiple executions
+
+    :param user: The user for which we are storing
+    :type user: User object
+    
+    :param friends: List of your likes
+    :type friends: list
+    '''
     from django_facebook.api import FacebookUserConverter
     logger.info('celery is storing %s likes' % len(likes))
     FacebookUserConverter._store_likes(user, likes)
@@ -17,6 +27,15 @@ def get_and_store_likes(user, facebook):
     '''
     Since facebook is quite slow this version also runs the get
     on the background
+    
+    Inserting again will not cause any errors, so this is safe
+    for multiple executions
+
+    :param user: The user for which we are storing
+    :type user: User object
+    
+    :param facebook: The graph connection to facebook
+    :type facebook: FacebookUserConverter object
     '''
     try:
         logger.info('attempting to get and store friends for %s', user.id)
@@ -29,6 +48,16 @@ def get_and_store_likes(user, facebook):
 
 @task.task(ignore_result=True)
 def store_friends(user, friends):
+    '''
+    Inserting again will not cause any errors, so this is safe
+    for multiple executions
+
+    :param user: The user for which we are storing
+    :type user: User object
+    
+    :param friends: List of your friends
+    :type friends: list
+    '''
     from django_facebook.api import FacebookUserConverter
     logger.info('celery is storing %s friends' % len(friends))
     FacebookUserConverter._store_friends(user, friends)
@@ -40,6 +69,15 @@ def get_and_store_friends(user, facebook):
     '''
     Since facebook is quite slow this version also runs the get
     on the background
+    
+    Inserting again will not cause any errors, so this is safe
+    for multiple executions
+
+    :param user: The user for which we are storing
+    :type user: User object
+    
+    :param facebook: The graph connection to facebook
+    :type facebook: FacebookUserConverter object
     '''
     try:
         logger.info('attempting to get and store friends for %s', user.id)
@@ -55,5 +93,8 @@ def async_connect_user(request, graph):
     '''
     Runs the whole connect flow in the background.
     Saving your webservers from facebook fluctuations
+    
+    Currently this has not yet been implemented.
+    It will be possible to run this command 1-N times
     '''
     pass
