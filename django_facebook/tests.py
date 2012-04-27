18e417cb55e75b6b41bf1a06ceae1e19b31bfa9d
Thierry Schellenbach <thierryschellenbach@gmail.com>
2012-04-27 10:30:10 +0200

    test improvements

--- django_facebook/tests.py
+++ django_facebook/tests.py
@@ -14,19 +14,13 @@ import logging
 from open_facebook.api import FacebookConnection
 from functools import partial
 from django_facebook.utils import cleanup_oauth_url
+from django_facebook.tests_utils.base import RequestMock
 
 logger = logging.getLogger(__name__)
 
 
 __doctests__ = ['django_facebook.api']
 
-
-'''
-TODO
-The views are currently untested,
-only the underlying functionality is.
-(need to fake facebook cookie stuff to correctly test the views)
-'''
 
 
 class UserConnectTest(FacebookTest):
@@ -36,15 +30,13 @@ class UserConnectTest(FacebookTest):
     fixtures = ['users.json']
 
     def test_persistent_graph(self):
-        from django.test import RequestFactory
-        request = RequestFactory()
+        request = RequestMock()
         request.session = {}
         request.user = AnonymousUser()
         get_persistent_graph(request, access_token='short_username')
         
     def test_gender_matching(self):
-        from django.test import RequestFactory
-        request = RequestFactory()
+        request = RequestMock()
         request.session = {}
         request.user = AnonymousUser()
         graph = get_persistent_graph(request, access_token='paul')
@@ -62,6 +54,9 @@ class UserConnectTest(FacebookTest):
         FacebookUserConverter(graph)
         action, user = connect_user(self.request, facebook_graph=graph)
         self.assertEqual(action, CONNECT_ACTIONS.REGISTER)
+        action, user = connect_user(self.request, facebook_graph=graph)
+        self.assertEqual(action, CONNECT_ACTIONS.LOGIN)
+        self.request.GET['connect_facebook'] = 1
         action, user = connect_user(self.request, facebook_graph=graph)
         self.assertEqual(action, CONNECT_ACTIONS.CONNECT)
         self.request.user = AnonymousUser()
@@ -152,11 +147,11 @@ class AuthBackend(FacebookTest):
 
 class ErrorMappingTest(FacebookTest):
     def test_mapping(self):
-        from open_facebook import exceptions as facebook_exceptions
+        from open_facebook import exceptions as open_facebook_exceptions
         raise_something = partial(FacebookConnection.raise_error, 0,
                                   "(#200) The user hasn't authorized the " \
                                   "application to perform this action")
-        self.assertRaises(facebook_exceptions.PermissionException,
+        self.assertRaises(open_facebook_exceptions.PermissionException,
                           raise_something)
 
 
