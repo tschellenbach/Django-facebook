11579b0463e78f57cb1b7fbbd20a2c46ac94d1a1
Thierry Schellenbach <thierryschellenbach@gmail.com>
2012-05-04 16:26:21 +0200

    settings changes

--- facebook_example/settings.py
+++ facebook_example/settings.py
@@ -12,6 +12,7 @@ BASE_ROOT = os.path.abspath(
 
 MANAGERS = ADMINS
 
+# Database config
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
