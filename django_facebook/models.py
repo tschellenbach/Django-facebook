8bd8460f2797bfe571269ad7e3173d1593cd1f34
Felipe Prenholato <philipe.rp@gmail.com>
2012-05-31 20:30:40 -0300

    Since forms can be reusable I added to next_redirect call on FacebookProfileModel.post_facebook_registration keys 'next' and 'redirect_uri' so 'register_next','next' and 'redirect_uri' can be detected in request to redirect after a register instead of only 'register_next'. This allow more customized forms too.

--- django_facebook/models.py
+++ django_facebook/models.py
@@ -52,7 +52,7 @@ class FacebookProfileModel(models.Model):
         from django_facebook.utils import next_redirect
         default_url = reverse('facebook_connect')
         response = next_redirect(request, default=default_url,
-                                 next_key='register_next')
+                                 next_key=['register_next','next','redirect_uri'])
         response.set_cookie('fresh_registration', self.user_id)
 
         return response
