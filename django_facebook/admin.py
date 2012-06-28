0d59c6d8149cb2c07a11eae68f3b46042c4f67e1
Stefano Crosta <stefano@slicefactory.com>
2012-06-28 17:29:32 +0200

    django-facebook admin shows profile when using default profile model

--- django_facebook/admin.py
+++ django_facebook/admin.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+
+
+
 from django.contrib import admin
 from django_facebook import models
-
 
 class FacebookUserAdmin(admin.ModelAdmin):
     list_display = ('user_id', 'name', 'facebook_id',)
@@ -12,6 +15,8 @@ class FacebookLikeAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     filter_fields = ('category', )
 
+if settings.AUTH_PROFILE_MODULE ==  'django_facebook.FacebookProfile':
+    admin.site.register(models.FacebookProfile)
 
 admin.site.register(models.FacebookUser, FacebookUserAdmin)
 admin.site.register(models.FacebookLike, FacebookLikeAdmin)
