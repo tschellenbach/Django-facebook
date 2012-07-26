05980e6edfb417315b8e1d387804b9344ac9b850
Thierry Schellenbach <thierryschellenbach@gmail.com>
2012-07-26 09:50:18 +0200

    why are there print statements in my code? should add a test for this

--- django_facebook/forms.py
+++ django_facebook/forms.py
@@ -45,7 +45,6 @@ class FacebookRegistrationFormUniqueEmail(forms.Form):
         match. Note that an error here will end up in
         ``non_field_errors()`` because it doesn't apply to a single
         field.
-        
         """
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
