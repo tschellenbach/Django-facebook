da8b005541b3d8fc702d6b372a909f30d2921b30
Thierry Schellenbach <thierryschellenbach@gmail.com>
2012-09-13 11:58:58 +0200

    fabfile shortcuts for validaiton and cleaning

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,13 @@
 from fabric.api import local
 
 
-def publish():
+def publish(validate='yes'):
     '''
     Easy publishing of my nice open source project
     '''
+    if validate == 'yes':
+        validate()
+    
     local('git push')
 
     from django_facebook import __version__
@@ -13,3 +16,14 @@ def publish():
 
     local('git tag %s' % tag_name)
     local('git push origin --tags')
+
+
+def validate():
+    local('pep8 --exclude=migrations --ignore=E501,E225 django_facebook open_facebook')
+    local('facebook_example\manage.py test django_facebook')
+    
+    
+def clean():
+    local('autopep8 -i *.py')
+    local('autopep8 -i django_facebook/*.py')
+    local('autopep8 -i open_facebook/*.py')
