00dcb30b753a1c6e16d9ae05de88a5d3fffa500a
gumuz <guyon.moree@gmail.com>
2012-09-24 19:54:32 +0200

    - fixed bug in celery delay syntax for extra params

--- django_facebook/tasks.py
+++ django_facebook/tasks.py
@@ -124,7 +124,7 @@ def retry_open_graph_shares_for_user(user):
 def token_extended_connect(sender, profile, token_changed, old_token, **kwargs):
     user = profile.user
     #make sure we don't have troubles caused by replication lag
-    retry_open_graph_shares_for_user.delay(user, countdown=60)
+    retry_open_graph_shares_for_user.apply_async(args=[user], countdown=60)
 
 from django_facebook.signals import facebook_token_extend_finished
 facebook_token_extend_finished.connect(token_extended_connect)
