1122502e43f6ef95d430dc176eac1c7685c25744
Thierry <thierryschellenbach@gmail.com>
2013-05-09 21:42:28 +0200

    fabfile changes

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 from fabric.api import local, cd
 import os
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-
+manage_py = os.path.join('facebook_example', 'manage.py')
 
 def publish(test='yes'):
     '''
@@ -24,7 +24,7 @@ def validate():
     with cd(PROJECT_ROOT):
         local(
             'pep8 --exclude=migrations --ignore=E501,E225 django_facebook open_facebook')
-        local('facebook_example\manage.py test open_facebook django_facebook')
+        local('%s test open_facebook django_facebook' % manage_py)
 
 
 def clean():
