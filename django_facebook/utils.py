cd609ce399d931d8796a03882d9cb0e3afb6c6ff
Thierry Schellenbach <thierryschellenbach@gmail.com>
2013-12-05 10:03:35 +0100

    pep8

--- django_facebook/utils.py
+++ django_facebook/utils.py
@@ -676,14 +676,14 @@ def get_migration_data():
     http://kevindias.com/writing/django-custom-user-models-south-and-reusable-apps/
     https://github.com/stephenmcd/mezzanine/blob/master/mezzanine/core/migrations/0005_auto__chg_field_sitepermission_user__del_unique_sitepermission_user.py
     '''
-    
+
     try:
         from django.contrib.auth import get_user_model
     except ImportError:  # django < 1.5
         from django.contrib.auth.models import User
     else:
         User = get_user_model()
-    
+
     user_orm_label = '%s.%s' % (User._meta.app_label, User._meta.object_name)
     user_model_label = '%s.%s' % (User._meta.app_label, User._meta.module_name)
     return User, user_orm_label, user_model_label
