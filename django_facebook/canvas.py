738b3ed2b3579c61f35778ee0bb8b293d63fa520
Cruceru Radu <radu.cruceru@gmail.com>
2014-11-12 13:11:30 +0200

    added FACEBOOK_APP_ID and FACEBOOK_APP_SECRET as kwargs

--- django_facebook/canvas.py
+++ django_facebook/canvas.py
@@ -3,11 +3,14 @@ from django_facebook import settings as facebook_settings
 
 
 def generate_oauth_url(scope=facebook_settings.FACEBOOK_DEFAULT_SCOPE,
-                       next=None, extra_data=None):
+                       next=None, extra_data=None, *args, **kwargs):
     query_dict = QueryDict('', True)
-    canvas_page = (next if next is not None else
-                   facebook_settings.FACEBOOK_CANVAS_PAGE)
-    query_dict.update(dict(client_id=facebook_settings.FACEBOOK_APP_ID,
+    facebook_app_id = kwargs.get(facebook_settings.FACEBOOK_APP_ID_KWARGS,
+                                facebook_settings.FACEBOOK_APP_ID)
+    facebook_canvas_page = kwargs.get(facebook_settings.FACEBOOK_CANVAS_PAGE_KWARGS,
+                                    facebook_settings.FACEBOOK_CANVAS_PAGE)
+    canvas_page = (next if next is not None else facebook_canvas_page)
+    query_dict.update(dict(client_id=facebook_app_id,
                            redirect_uri=canvas_page,
                            scope=','.join(scope)))
     if extra_data:
