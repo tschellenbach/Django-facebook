d212d3c957ba0b5d18533926abd1683ef62ef2b3
Thierry Schellenbach <thierryschellenbach@gmail.com>
2017-03-16 09:02:41 -0600

    Update api.py

--- open_facebook/api.py
+++ open_facebook/api.py
@@ -848,7 +848,7 @@ class OpenFacebook(FacebookConnection):
         '''
         me = getattr(self, '_me', None)
         if me is None:
-            self._me = me = self.get('me')
+            self._me = me = self.get('me', fields="id,name,email,verified")
 
         return me
 
