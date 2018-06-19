8fe466faf8c4bb50c896d09d6a81b3e65ce566a4
Jack Laxson <jackjrabbit@gmail.com>
2018-06-19 10:46:07 -0400

    bump up middlewares to 2.0 defaults

--- facebook_example/facebook_example/settings.py
+++ facebook_example/facebook_example/settings.py
@@ -129,13 +129,15 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
-MIDDLEWARE = (
-    'django.middleware.common.CommonMiddleware',
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-)
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 ROOT_URLCONF = 'facebook_example.urls'
 
